a00e6f4c141272f696b91c81051abf949a10b77f
mery-wq <mery@gmail.com>
2024-03-24 00:54:34 +0900

    add synbolic link script.

---
+++ makeLinks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# synbolic links
+DOTFILES_DIR=$(dirname "$0")
+
+cd $DOTFILES_DIR
+
+ln -sf $DOTFILES_DIR/.bashrc $HOME/.bashrc
+ln -sf $DOTFILES_DIR/.tmux.conf $HOME/.tmux.conf
+ln -sf $DOTFILES_DIR/.zshrc $HOME/.zshrc
+ln -sf $DOTFILES_DIR/.config $HOME/.config
+
+echo "Dotfiles have been symlinked."
