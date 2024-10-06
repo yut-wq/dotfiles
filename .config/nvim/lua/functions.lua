00d8f62d557ca54553cb6d22dc5881141516673c
yut-wq <println856@gmail.com>
2024-10-06 16:50:38 +0900

    fix: if file type is markdown, not trim white space.

--- .config/nvim/lua/functions.lua
+++ .config/nvim/lua/functions.lua
@@ -11,7 +11,14 @@ end
 vim.keymap.set({ "n" }, "<leader><leader>f", auto_format)
 
 -- 行末の空白を削除する。
+local function trim_white_space()
+	local filetype = vim.bo.filetype
+	if filetype ~= "markdown" then
+		vim.command([[%s/\s\+$//ge]])
+	end
+end
+
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	pattern = { "*" },
-	command = [[:%s/\s\+$//ge]],
+	callback = trim_white_space
 })
