aab5b6b14be03122fef3ca1191edcb1c1a3c2bb2
yut-wq <println856@gmail.com>
2024-10-06 17:51:42 +0900

    feat: add nvim-surround plugin.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -99,6 +99,16 @@ require("lazy").setup({
 			-- see below for full list of optional dependencies 👇
 		},
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
+	}
 })
 
 -- telescope.nvim
