0afbc4d243397268ee1d21981e78aa47bbb71b2b
yut-wq <println856@gmail.com>
2025-02-11 11:17:20 +0900

    zshのvimモードを削除

--- .config/zsh/plugin.zsh
+++ .config/zsh/plugin.zsh
@@ -3,7 +3,3 @@ zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 bindkey '^j' autosuggest-accept
 zinit ice depth=1
-zinit light jeffreytse/zsh-vi-mode
-ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
-# zvm_bindkey <space>s ^
-# zvm_bindkey <space>e $
