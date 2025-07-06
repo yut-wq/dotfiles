548056fa6a3ba4702d39d93ee448ad238646a7c7
yut-wq <println856@gmail.com>
2025-07-06 14:14:17 +0900

    <CR>削除のショートカットを追加

--- .config/nvim/lua/functions.lua
+++ .config/nvim/lua/functions.lua
@@ -6,6 +6,9 @@ local function trim_white_space()
 	end
 end
 
+-- <CR>の削除ショートカット
+vim.keymap.set("n", "<leader>rm", [[:silent! %s/\r$//<CR>]], { noremap = true, silent = true })
+
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	pattern = { "*" },
 	callback = trim_white_space,
