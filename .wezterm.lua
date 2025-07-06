21e5bd3a37ff9d854ef2e65cf231f8ea4ede567a
yut-wq <println856@gmail.com>
2025-07-06 18:47:24 +0900

    不要なコメントの削除

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,13 +4,7 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-
--- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
-
--- or, changing the font size and color scheme.
+-- global
 config.font = wezterm.font("UDEV Gothic NF")
 config.font_size = 14
 config.color_scheme = "Material (base16)"
