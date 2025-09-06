164d14bfcd48e7e84b4c688f6908ab7a14c23d73
yut-wq <println856@gmail.com>
2025-09-06 13:46:18 +0900

    markdown以外のlinter設定を削除

--- .config/nvim/lua/plugins/linter.lua
+++ .config/nvim/lua/plugins/linter.lua
@@ -6,10 +6,7 @@ return {
 			-- Event to trigger linters
 			events = { "BufWritePost", "BufReadPost", "InsertLeave" },
 			linters_by_ft = {
-				rust = { "clippy", "cspell" },
 				markdown = { "cspell" },
-				javascript = { "cspell", "eslint_d" },
-				typescript = { "cspell", "eslint_d" },
 			},
 		},
 		config = function(_, opts)
