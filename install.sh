0b7c041f6a0fd0958f2e01c095540732a5407d08
yut-wq <println856@gmail.com>
2025-09-06 17:28:23 +0900

    installerの追加

---
+++ install.sh
@@ -0,0 +1,13 @@
+# !/usr/bin/env bash
+
+# 依存関係インストール用スクリプト
+
+# uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# pyenv
+curl -fsSL https://pyenv.run | sh
+
+# rg
+sudo apt-get install ripgrep
+
