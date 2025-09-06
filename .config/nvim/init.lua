556fd2ca989f431f85540cef639a77de65f3e1da
yut-wq <89442863+yut-wq@users.noreply.github.com>
2025-09-06 06:58:34 +0000

    Apply automatic changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,25 +4,6 @@ require("functions")
 -- plugins
 require("pluginManager")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 vim.opt.termguicolors = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
