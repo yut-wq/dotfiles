a0c9cc45e4c849bc9342a62ee6ddc9d033cf2d8c
yut-wq <println856@gmail.com>
2025-10-26 03:20:44 +0900

    相対パスを表示するように

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,15 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
-		opts = {},
+		opts = {
+			sections = {
+				lualine_c = {
+					{
+						'filename',
+						path = 1, -- 0: ファイル名のみ, 1: 相対パス, 2: 絶対パス, 3: 絶対パス(~付き)
+					}
+				}
+			}
+		},
 	},
 }
