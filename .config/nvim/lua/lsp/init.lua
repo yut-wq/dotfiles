b2a8b6a98dbd77988dd656bf578cf58c2b536d37
yut-wq <println856@gmail.com>
2025-10-26 03:17:48 +0900

    svelteを追加

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -47,8 +47,5 @@ local lua_ls_opts = require("lsp.lua_ls")
 vim.lsp.config("lua_ls", lua_ls_opts)
 vim.lsp.enable("lua_ls")
 
-vim.lsp.config("rust_analyzer", {
-	cmd = { "rust-analyzer" },
-	filetypes = { "rust" },
-})
 vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("svelte")
